affafb68f6a6caee4f467bd1d4b1e3bb9e7af33c
Don Hamilton III <donhamiltoniii@gmail.com>
2019-08-22 14:41:25 -0400

    Finish testing DeckGenerator

---
+++ DeckGenerator.test.ts
@@ -0,0 +1,22 @@
+const DeckGenerator = require('./DeckGenerator')
+
+describe('DeckGenerator', () => {
+
+  test('given spades as a suit and 2 and 3 as values, should generate a deck of 2 cards', () => {
+    const underTest = new DeckGenerator()
+
+    const deck = underTest.generate(['♤'], ['2', '3'])
+
+    expect(deck).toContain('2♤')
+    expect(deck).toContain('3♤')
+  })
+
+  test('given 🔴 as a suit and 2 and 3 as values, should generate a deck of 2 cards', () => {
+    const underTest = new DeckGenerator()
+
+    const deck = underTest.generate(['🔴'], ['2', '3'])
+
+    expect(deck).toContain('2🔴')
+    expect(deck).toContain('3🔴')
+  })
+})
---
+++ DeckGenerator.ts
@@ -0,0 +1,13 @@
+module.exports = class DeckGenerator {
+  generate(suits: String[], values: String[]): String[] {
+    let deck: String[] = []
+
+    suits.forEach(suit => {
+      values.forEach(value => {
+        deck.push(`${value}${suit}`)
+      })
+    })
+
+    return deck
+  }
+}
