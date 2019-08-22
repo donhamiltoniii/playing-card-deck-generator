82f2b5e3011d1cfbfce190ab9fb6318598461291
Don Hamilton III <donhamiltoniii@gmail.com>
2019-08-22 17:05:35 -0400

    Move code to 'lib'

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import DeckGenerator from './src/DeckGenerator'
+import DeckGenerator from './lib/DeckGenerator'
 
 const standardDeck: String[] = new DeckGenerator().generate(['♤', '♡', '♧', '♢'], ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'])
 
