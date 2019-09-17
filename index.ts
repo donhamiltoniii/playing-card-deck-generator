fe2979a91bbf491100259509060a334365ddb7b4
Don Hamilton III <donhamiltoniii@gmail.com>
2019-09-17 09:28:41 -0400

    Fix standard deck and expose new Types for usage outside package

--- index.ts
+++ index.ts
@@ -1,9 +1,11 @@
 import DeckGenerator from './lib/DeckGenerator'
+import Deck from './lib/Deck'
+import Card from './lib/Card'
 
-const standardDeck: String[] = new DeckGenerator().generate(
+const standardDeck: DeckGenerator = new DeckGenerator(
   ['♤', '♡', '♧', '♢'],
   ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
 )
 
-export { standardDeck }
+export { Card, Deck, standardDeck }
 export default DeckGenerator
