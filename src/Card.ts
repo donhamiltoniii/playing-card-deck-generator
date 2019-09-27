4d5a57b0f4628374f1e48a177618413d8afe4965
Don Hamilton III <donhamiltoniii@gmail.com>
2019-09-26 22:21:35 -0400

    Fix accessability issue with Card

--- src/Card.ts
+++ src/Card.ts
@@ -2,13 +2,21 @@ class Card {
   private suit: string
   private value: string
 
+  private get Suit(): string {
+    return this.suit
+  }
+
   public getValue(): string {
-    return `${this.value}${this.suit}`
+    return this.value
   }
 
   constructor(value: string, suit: string) {
     this.suit = suit
     this.value = value
+  }
+
+  public getCardValue(): string[] {
+    return [this.value, this.suit]
   }
 }
 
